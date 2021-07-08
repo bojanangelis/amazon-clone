1ab1894d569f050f213393ace4dc1ae94dd325a0
Bojan Angjeleski <76458601+bojanangelis@users.noreply.github.com>
2021-07-08 16:53:40 +0200

    update

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJUD7TWR37IL7ovV_Ct-HLkXF3brUlfCE",
+  authDomain: "amzm-2f6c3.firebaseapp.com",
+  projectId: "amzm-2f6c3",
+  storageBucket: "amzm-2f6c3.appspot.com",
+  messagingSenderId: "564759786857",
+  appId: "1:564759786857:web:d9a9373262caf34f3d39f8",
+};
